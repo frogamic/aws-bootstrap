708c20fc7518d8b0c82dfe453bc3d7fb62276557
Dominic Shelton <frogamic@protonmail.com>
2021-12-06 22:20:28 +1100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o errtrace
+
+die() {
+	echo "$*" 1>&2
+	exit 1
+}
+
+cd "$(dirname "$BASH_SOURCE[0]")"
+
+PROJECT="bootstrap"
+STAGE="prod"
+REVISION="$(git rev-parse --short HEAD || die "Unknown git revision")"
+REGION="ap-southeast-2"
+
+cd templates
+for TEMPLATE in *.yml; do
+	STACKNAME="${PROJECT}-${TEMPLATE%.yml}-${STAGE}"
+	echo "Deploying ${STACKNAME}"
+
+	aws cloudformation deploy \
+		--stack-name "$STACKNAME" \
+		--template-file "$TEMPLATE" \
+		--capabilities CAPABILITY_NAMED_IAM \
+		--region "$REGION" \
+		--parameter-overrides \
+			"Project=${PROJECT}" \
+			"Stage=${STAGE}" \
+			"Revision=${REVISION}"
+done
